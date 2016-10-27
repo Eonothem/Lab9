71ae2ce5d6933f049d51bde04060648cefc9ead0
tkitson <tkitson@udel.edu>
2016-10-27 18:37:21 -0400

    vc

--- lab9.rkt
+++ lab9.rkt
@@ -9,15 +9,29 @@
 ;;  - AtomicValue
 ;;  - [FunctionCallOf X]
 
-;; An AtomicValue is one of:
-;;  - Boolean
-;;  - Number
-;;  - String
-;;  - Image
+;;Template:
+#; (define (expression-fn expr)
+     (cond [(struct? expr) ... (X-fn expr)]
+           [(atom? expr) ...]))
+
+;; An AtomicValue is any Expresion which statisfies the atom? predicate
 
 ;; A [FunctionCallOf X] is a (make-X arg1 arg2),
 ;;  where X is the structure representation of the function being called
 ;;  Interpertation:
 ;;   - Expression arg1: the first arguement passed to X when X is executed
 ;;   - expression arg2: the first arguement passed to X when X is executed
+
+;;Examples:
+(define-struct add [arg1 arg2])
+(define-struct mul [arg1 arg2])
+(define FC-0 (make-mul 3 10))
+(define FC-1 (make-add (make-mul 3 3) (make-mul 4 4)))
+(define FC-2 (make-add (make-mul 'x 'x) (make-mul 4 4)))
+(define FC-3 (make-mul 0.5 (make-mul 3 3)))
+
+;;Template:
+#; (define (function-call-fn fc)
+     ... (X-arg1 fc)
+     ... (X-arg2 fc))
 
