11b721e782c184623236b7bb58bf656369a98489
tkitson <tkitson@udel.edu>
2016-10-27 18:26:06 -0400

    Problem 111111111111111

--- lab9.rkt
+++ lab9.rkt
@@ -1,3 +1,23 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname lab9) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+;;=============================================================================
+;;Problem 1:
+;;=============================================================================
+
+;; An Expresion is one of:
+;;  - AtomicValue
+;;  - [FunctionCallOf X]
+
+;; An AtomicValue is one of:
+;;  - Boolean
+;;  - Number
+;;  - String
+;;  - Image
+
+;; A [FunctionCallOf X] is a (make-X arg1 arg2),
+;;  where X is the structure representation of the function being called
+;;  Interpertation:
+;;   - Expression arg1: the first arguement passed to X when X is executed
+;;   - expression arg2: the first arguement passed to X when X is executed
+
