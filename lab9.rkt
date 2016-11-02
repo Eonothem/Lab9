ea4b955547bfc760088f4f14e91671cb230cf9a2
tkitson <tkitson@udel.edu>
2016-11-02 18:31:01 -0400

    nom nom nom
    aka nombres

--- lab9.rkt
+++ lab9.rkt
@@ -1,6 +1,10 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname lab9) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+;; Connor Onweller
+;; James Skripchuk
+;; Thomas Kitson
+
 ;;=============================================================================
 ;;Problem 1:
 ;;=============================================================================
